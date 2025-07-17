fa6813d11c5371fc0351991e5c77bc0cbfedba2b
Fabian Sanchez <fabiansanchezd@outlook.com>
2025-07-16 22:14:43 -0600

    path parameters, query parameters

--- learning-fast-api/working.py
+++ learning-fast-api/working.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return {"Data": "Testing"}
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 4,
+        "brand": 'Dos Pinos'
+    }
+}
+
+@app.get('/get-item/{item_id}')
+def get_item(item_id: int = Path(..., description="The ID of the item you'd like to view", gt = 0, lt = 2)): #like in ts
+    return inventory[item_id]
+
+@app.get('/get-by-name/{item-id}')
+def get_item(*, item_id: int, test:int , name: Optional[str] = None):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+        
+    return {"Data not found"}
