da1a6fb9de9b633bc0ff9924df2a632b2103717e
Fabian Sanchez <fabiansanchezd@outlook.com>
2025-07-28 23:53:18 -0600

    first endpoints

--- backend/chapter_2/server.js
+++ backend/chapter_2/server.js
@@ -3,4 +3,15 @@ const app = express();
 
 const PORT = 1212;
 
-app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
+app.get('/', (req, res) => {
+    // endpoint #1, /
+    console.log("hit endpoint #1", req.method)
+    res.sendStatus(200)
+})
+
+app.get('/dashboard', (req, res) => {
+    console.log('hit endpoint #2', req.method)
+    res.send('hello')
+})
+
+app.listen(PORT, () => console.log(`Server has started on ${PORT}`));   
