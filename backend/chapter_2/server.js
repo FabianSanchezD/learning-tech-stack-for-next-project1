c1deda39e51d2444288f896037a2bdb152efa785
Fabian Sanchez <fabiansanchezd@outlook.com>
2025-07-29 11:34:27 -0600

    post, delete endpoints

--- backend/chapter_2/server.js
+++ backend/chapter_2/server.js
@@ -6,12 +6,41 @@ const PORT = 1212;
 app.get('/', (req, res) => {
     // endpoint #1, /
     console.log("hit endpoint #1", req.method)
-    res.sendStatus(200)
+    res.send(`<body>
+                <h1>DATA</h1>
+                <p>${JSON.stringify(data)}</p>
+            </body>`)
 })
 
 app.get('/dashboard', (req, res) => {
     console.log('hit endpoint #2', req.method)
     res.send('hello')
+})
+
+let data = ['fabian']
+
+//Middleware
+
+app.use(express.json())
+
+// api endpoints
+
+app.get('/api/data', (req, res) => {
+    console.log('this is for data')
+    res.send(data)
+})
+
+app.post('/api/data', (req, res) => {
+    const newEntry = req.body
+    console.log(newEntry)
+    data.push(newEntry.name)
+    res.sendStatus(201)
+})
+
+app.delete('/api/data', (req, res) => {
+    data.pop()
+    console.log('eliminated')
+    res.sendStatus(200)
 })
 
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`));   
